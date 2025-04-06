c5aa134133e1de2cbb27bcbb186d02b0fdac50a7
ArseniiKlimenko <aklimenko@kse.org.ua>
2025-04-06 17:16:43 +0300

    begging of work classes graph and matrixgraph

---
+++ DiscreteMath/DiscreteMath/Graph.cs
@@ -0,0 +1,54 @@
+﻿namespace DiscreteMath;
+
+public class Graph
+{
+    public int NumVertices { get; private set; }
+    public List<int>[] AdjacencyList { get; private set; }
+
+    public Graph(int numVertices)
+    {
+        NumVertices = numVertices;
+        AdjacencyList = new List<int>[numVertices];
+        for (int i = 0; i < numVertices; i++)
+        {
+            AdjacencyList[i] = new List<int>();
+        }
+    }
+    
+    public void AddEdge(int u, int v)
+    {
+        AdjacencyList[u].Add(v);
+    }
+    
+    public void RemoveEdge(int u, int v)
+    {
+        AdjacencyList[u].Remove(v);
+    }
+    
+    public void DisplayList()
+    {
+        Console.WriteLine("Списки суміжності:");
+        for (int i = 0; i < NumVertices; i++)
+        {
+            Console.Write($"{i}: ");
+            foreach (var vertex in AdjacencyList[i])
+            {
+                Console.Write($"{vertex} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public GraphMatrix ToGraphMatrix()
+    {
+        GraphMatrix matrixGraph = new GraphMatrix(NumVertices);
+        for (int u = 0; u < NumVertices; u++)
+        {
+            foreach (int v in AdjacencyList[u])
+            {
+                matrixGraph.AddEdge(u, v);
+            }
+        }
+        return matrixGraph;
+    }
+}
---
+++ DiscreteMath/DiscreteMath/GraphMatrix.cs
@@ -0,0 +1,52 @@
+﻿namespace DiscreteMath;
+
+public class GraphMatrix
+{
+    public int NumVertices { get; private set; }
+    public int[,] Matrix { get; private set; }
+
+    public GraphMatrix(int numVertices)
+    {
+        NumVertices = numVertices;
+        Matrix = new int[numVertices, numVertices];
+    }
+    
+    public void AddEdge(int u, int v)
+    {
+        Matrix[u, v] = 1;
+    }
+    
+    public void RemoveEdge(int u, int v)
+    {
+        Matrix[u, v] = 0;
+    }
+    
+    public void DisplayMatrix()
+    {
+        Console.WriteLine("Матриця суміжності:");
+        for (int i = 0; i < NumVertices; i++)
+        {
+            for (int j = 0; j < NumVertices; j++)
+            {
+                Console.Write(Matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+    public Graph ToGraphList()
+    {
+        Graph graph = new Graph(NumVertices);
+        for (int i = 0; i < NumVertices; i++)
+        {
+            for (int j = 0; j < NumVertices; j++)
+            {
+                if (Matrix[i, j] != 0)
+                {
+                    graph.AdjacencyList[i].Add(j);
+                }
+            }
+        }
+        return graph;
+    }
+}
