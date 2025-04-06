8d3e7cbf4a1314ad5bdd76952a9f8aaafef613ef
ovalova <ovalova@kse.org.ua>
2025-04-06 18:58:52 +0300

    Update Program.cs

--- DiscreteMath/DiscreteMath/Program.cs
+++ DiscreteMath/DiscreteMath/Program.cs
@@ -2,9 +2,25 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int n = 6;
+            double density = 0.5;
+
+            Graph graph = GraphGenerator.GenerateRandomDAG(n, density);
+            graph.DisplayList(); 
+
+            
+            TopologicalSorter sorter = new TopologicalSorter();
+            List<int> sorted = sorter.Sort(graph);
+
+            Console.WriteLine("\nТопологічне сортування :");
+            Console.WriteLine(string.Join(" → ", sorted));
+            Graph testGraph = new Graph(4);
+            testGraph.AddEdge(0, 1);
+            testGraph.AddEdge(1, 2);
+            testGraph.AddEdge(2, 3);
         }
+
     }
 }
